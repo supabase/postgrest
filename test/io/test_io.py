5c6dd37d012d22b9e12ec48d8056a52996037ffe
Taimoor Zaeem <taimoorzaeem@gmail.com>
2026-02-17 10:48:16 +0500

    test(io): increase statement_timeout value of anonymous role
    It is too low which leaves a small window of values to
    use when testing other features.
    
    Signed-off-by: Taimoor Zaeem <taimoorzaeem@gmail.com>

--- test/io/test_io.py
+++ test/io/test_io.py
@@ -941,11 +941,11 @@ def test_role_settings(defaultenv):
     with run(env=env) as postgrest:
         # statement_timeout for postgrest_test_anonymous
         response = postgrest.session.get("/rpc/get_guc_value?name=statement_timeout")
-        assert response.text == '"2s"'
+        assert response.text == '"5s"'
 
         # reload statement_timeout with NOTIFY
         response = postgrest.session.post(
-            "/rpc/change_role_statement_timeout", data={"timeout": "5s"}
+            "/rpc/change_role_statement_timeout", data={"timeout": "8s"}
         )
         assert response.text == ""
         assert response.status_code == 204
@@ -956,7 +956,7 @@ def test_role_settings(defaultenv):
         sleep_until_postgrest_config_reload()
 
         response = postgrest.session.get("/rpc/get_guc_value?name=statement_timeout")
-        assert response.text == '"5s"'
+        assert response.text == '"8s"'
 
         # statement_timeout for postgrest_test_author
         headers = jwtauthheader({"role": "postgrest_test_author"}, SECRET)
